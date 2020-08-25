833f092ba0defa8eb1795170facde796ec8f5a55
James Rowe <jamesrowe@outlook.com>
2020-08-25 15:50:36 -0400

    order of caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/style.css",
-  "/index.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-192x192.png",
   "/db.js",
-  "manifest.webmanifest"
+  "/index.js",
+  "manifest.webmanifest",
+  "/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",  
 ];
 
 const STATIC_CACHE = "static-cache-v1";
